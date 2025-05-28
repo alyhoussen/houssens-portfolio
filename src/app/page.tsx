83cd42a1ff8f3283015ed3b32858894a7c2fb269
alyhoussen <oussenalyjoma@gmail.com>
2025-05-28 18:42:04 +0200

    Deleted border

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,7 +18,7 @@ export default function Page() {
         <div className="mx-auto w-full max-w-2xl space-y-8">
           <div className="gap-2 md:flex flex-col justify-between">
             <BlurFade delay={BLUR_FADE_DELAY}>
-              <Avatar className="size-[300px] border">
+              <Avatar className="size-[300px]">
                 <AvatarImage alt={DATA.name} src={DATA.avatarUrl} />
                 <AvatarFallback>{DATA.initials}</AvatarFallback>
               </Avatar>
