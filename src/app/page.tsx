bfc29e30dbcdd9dbe9de8f7ecec5d8be96bf6a12
Houssen D. Aly <159697794+alyhoussen@users.noreply.github.com>
2025-06-11 19:14:27 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,7 +18,7 @@ export default function Page() {
         <div className="mx-auto w-full max-w-2xl space-y-8">
           <div className="gap-2 md:flex flex-col justify-between items-center">
             <BlurFade delay={BLUR_FADE_DELAY}>
-              <Avatar className="size-[150px] border mb-[25px]">
+              <Avatar className="size-[150px] border mb-[25px] flex justify-center items-center">
                 <AvatarImage alt={DATA.name} src={DATA.avatarUrl} className="size-[200px] stretch" />
                 <AvatarFallback>{DATA.initials}</AvatarFallback>
               </Avatar>
